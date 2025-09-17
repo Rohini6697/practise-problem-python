88a1e315ef5aabcdfd9c18ac28b9b1e84c06aea8
Rohini Sureshbabu <rohinimrd@gmail.com>
2025-09-17 12:18:29 +0530

    another commit

---
+++ miniproject/password_manager/manager.py
@@ -0,0 +1,57 @@
+with open('passwords.txt','r') as f:
+    passwords = f.readlines()
+    print(passwords)
+
+    for line in passwords:
+        parts = line.strip().split(',')
+        if len(parts) == 3:
+            website,username,password = parts
+            # print(website)
+
+
+
+def add_credentials():
+    with open('passwords.txt','a') as f:
+        add_website = input('enter website name : ')
+        add_username = input('enter usename : ')
+        add_password = int(input('enter password : '))
+        print()
+        add_data = f'{add_website},{add_username},{add_password}'
+        f.write(add_data+'\n')
+
+
+def veiw_credentilas():
+    print()
+    print(f'website : {website}')
+    print(f'username : {username}')
+    print(f'password : {password}')
+
+def search_credentials():
+    with open('passwords.txt','r'):
+        print()
+        search = input('search : ')
+        if search.lower() in website.lower():
+            print('website found')
+            print()
+            print(f'website : {website}')
+            print(f'user name : {username}')
+            print(f'password : {passwords}')
+            print()
+while True:
+    print('1. Add new credentials')
+    print('2. View credentials for a specific website')
+    print('3. View all stored credentials')
+    print('4. Exit the program')
+    print()
+
+
+    choice = input('enter your choice : ')
+
+    if choice == '1':
+        add_credentials()
+    elif choice == '2':
+        search_credentials()
+    elif choice == '3':
+        veiw_credentilas()
+    elif choice == '4':
+        break
